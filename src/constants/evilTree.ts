3412c0355d0987fea74ca0ea4871ffd7360f2b59
Ectropy <atomicfusion0@gmail.com>
2026-02-20 22:26:21 -0500

    FIX(constants): improve hint and location wording

--- src/constants/evilTree.ts
+++ src/constants/evilTree.ts
@@ -60,84 +60,84 @@ export interface LocationHint {
 
 export const LOCATION_HINTS: LocationHint[] = [
   {
-    hint: 'Close to a collection of yew trees',
-    locations: ["South of Seers' Village flax field"],
+    hint: 'Close to a collection of yew trees (Seers)',
+    locations: ["South of Seers' Village flax field by the yew trees"],
   },
   {
-    hint: 'Close to a dungeon entrance, tropical jungle',
-    locations: ['Ape Atoll', 'Brimhaven Dungeon'],
+    hint: 'Close to a dungeon entrance, and within a tropical jungle',
+    locations: ['Ape Atoll, near the entrance to Ape Atoll Dungeon', 'Near the entrance of the Brimhaven Dungeon'],
   },
   {
-    hint: 'Close to large willow collection',
+    hint: 'Close to a large collection of willow trees',
     locations: [
-      "Northwest Seers' Village bank",
-      'South Draynor Village',
-      "South Legends' Guild",
-      'Southwest Tree Gnome Stronghold',
+      "Northwest of Seers' Village bank",
+      'South of Draynor Village, near the willow trees',
+      "South of Legends' Guild",
+      "Southwest part of Tree Gnome Stronghold, north of Brimstail's cave and east of the bridge",
     ],
   },
   {
-    hint: 'Close to mine on city outskirts',
-    locations: ["South Legends' Guild", 'West Falador'],
+    hint: 'Close to a mine on the outskirts of a city',
+    locations: ["South of Legends' Guild", 'West of Falador'],
   },
   {
-    hint: 'Close to Runecrafting altar',
+    hint: 'Close to a Runecrafting altar',
     locations: [
-      'Near body altar south Edgeville Monastery',
-      'Southwest nature altar mining spot',
+      'Near the body altar, south of the Edgeville Monastery',
+      'Southwest of mining spot near the nature altar, northwest of Shilo Village',
     ],
   },
   {
-    hint: "Close to Legends' home",
-    locations: ["North Legends' Guild", "South Legends' Guild"],
+    hint: "Close to the home of 'Legends'",
+    locations: ["North of Legends' Guild", "South of Legends' Guild"],
   },
   {
-    hint: 'Close to Yanille',
-    locations: ['Northeast Yanille', 'South Tree Gnome Village'],
+    hint: 'Close to the town you call Yanille',
+    locations: ['Northeast of Yanille', 'South of Tree Gnome Village, northwest of Yanille'],
   },
   {
-    hint: "Close to Seers' Village",
-    locations: ["Inside McGrubor's Wood", 'Northwest bank', 'South flax field'],
+    hint: "Close to the village you call 'Seers'",
+    locations: ["Inside McGrubor's Wood", "Northwest of Seers' Village bank", "South of Seers' Village flax field by the yew trees"],
   },
   {
-    hint: 'Close to Draynor',
-    locations: ['North near Draynor Manor', 'South near willows'],
+    hint: "Close to the village you humans call 'Draynor'",
+    locations: ['North of Draynor Village, near Draynor Manor', 'South of Draynor Village, near the willow trees['],
   },
   {
-    hint: 'Due west of Lumbridge',
-    locations: ['South Draynor near willows'],
+    hint: 'Due west of the town you call Lumbridge',
+    locations: ['South of Draynor Village near the willow trees'],
   },
   {
-    hint: 'Rare trees with ogres nearby',
-    locations: ['South Castle Wars', "West Oo'glog"],
+    hint: 'In a location with rare trees and ogres nearby',
+    locations: ['South of Castle Wars, north of the fairy ring BKP', "West of Oo'glog"],
   },
   {
-    hint: 'Lands inhabited by elves',
-    locations: ['Northeast Tyras Camp', 'Lletya south magic trees'],
+    hint: 'In the lands inhabited by elves ',
+    locations: ['Northeast of Tyras Camp', 'Lletya, south of the magic trees'],
   },
   {
-    hint: 'Just outside Varrock',
-    locations: ["King's Road southeast lumber yard", 'North Varrock Palace'],
+    hint: 'Just outside of the city you call Varrock',
+    locations: ["King's Road, Southeast of Fort Forinthry and above digsite", 'North of Varrock Palace, near the Wilderness wall'],
   },
   {
-    hint: "North from Seers' Village",
-    locations: ['East Rellekka house portal', 'Northwest bank'],
+    hint: "North as the crow flies from Seers' Village",
+    locations: ['East of the house portal in Rellekka', "Northwest of Seers' Village bank"],
   },
   {
-    hint: 'North from Ardougne market',
-    locations: ["Inside McGrubor's Wood", 'Southwest Ranging Guild'],
+    hint: 'North as the crow flies from the market of Ardougne',
+    locations: ["Inside McGrubor's Wood", 'Southwest of the Ranging Guild'],
   },
   {
-    hint: 'On Karamja island',
-    locations: ['East Kharazi Jungle teaks', 'Southwest nature altar mining spot'],
+    hint: 'On the island known as Karamja',
+    locations: ['East of the teak trees in the Kharazi Jungle', 'Southwest of mining spot near the nature altar, northwest of Shilo Village'],
   },
   {
-    hint: 'Southern tropical island coast',
-    locations: ['Ape Atoll dungeon entrance', 'East Kharazi Jungle teaks'],
+    hint: 'On the southern coast of a tropical island',
+    locations: ['Ape Atoll, near the entrance to Ape Atoll Dungeon', 'Kharazi Jungle, east of the teak trees'],
   },
   {
-    hint: 'South of tree gnome settlement',
-    locations: ['South Tree Gnome Stronghold', 'South Tree Gnome Village'],
+    hint: 'To the south of a tree gnome settlement',
+    locations: ['South of Tree Gnome Stronghold and east of the Outpost', 'South of Tree Gnome Village and northwest of Yanille'],
   },
 ];
 
